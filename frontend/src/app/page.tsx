ea7143b981156afebebfa8db0099e46e7d1ca2f9
Matthew Favela <92203491+Chewy42@users.noreply.github.com>
2024-03-02 00:56:22 -0800

    Initial Commit

---
+++ frontend/src/app/page.tsx
@@ -0,0 +1,10 @@
+
+export default function Home() {
+  return (
+    <section>
+      <div className="w-screen h-screen flex justify-center align-middle">
+        <h1 className="mx-auto my-auto">Get ready to manage your employees.</h1>
+      </div>
+    </section>
+  );
+}
